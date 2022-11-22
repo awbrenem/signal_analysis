2a9e5a8d6063a4b4f80f215fad37f9c8d03c368d
awbrenem <78624201+awbrenem@users.noreply.github.com>
2022-11-22 12:42:57 -0500

    initial commit

---
+++ bandpass_data_butter.py
@@ -0,0 +1,23 @@
+"""
+Routine for bandpassing waveform data
+From https://stackoverflow.com/questions/12093594/how-to-implement-band-pass-butterworth-filter-with-scipy-signal-butter
+
+"""
+
+
+from scipy.signal import butter, sosfilt, sosfreqz
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        sos = butter(order, [low, high], analog=False, btype='band', output='sos')
+        return sos
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+        sos = butter_bandpass(lowcut, highcut, fs, order=order)
+        y = sosfilt(sos, data)
+        return y
+
+
