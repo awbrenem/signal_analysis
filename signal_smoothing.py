a1ca9f286e9170f09c0c5bd5a733b4283236ef7c
awbrenem <78624201+awbrenem@users.noreply.github.com>
2022-11-23 14:49:29 -0500

    minor changes

--- signal_smoothing.py
+++ signal_smoothing.py
@@ -2,9 +2,8 @@
 Smoothing routines for Python 
 
 Routines: 
-savgol_filter 
-sliding_box_avg
-
+savgol_filter (savgol_filter)
+sliding box average (sliding_box_avg)
 """
 
 
@@ -15,10 +14,10 @@ https://stackoverflow.com/questions/20618804/how-to-smooth-a-curve-in-the-right-
 e.g. 
 x = np.linspace(0,2*np.pi,100)
 y = np.sin(x) + np.random.random(100) * 0.2
-ysmoo = savitzky_golay(y, 51, 3)
+ysmoo = savgol_filter(y, window=50, poly=3)
 
 """
-def savgol_filter(data,window=51,poly=3):
+def savgol_filter(data,window=50,poly=3):
     from scipy.signal import savgol_filter    
     #savgol_filter(data, window_size, poly_order)
     return savgol_filter(data, window, poly) 
@@ -33,7 +32,7 @@ E.g.:
 x = np.linspace(0,2*np.pi,100)
 y = np.sin(x) + np.random.random(100) * 0.8
 box_pts = 3
-ysmoo = sliding_box_avg(y,box_pts)
+ysmoo = sliding_box_avg(y,box_pts=10)
 """
 
 def sliding_box_avg(data, box_pts=3):
