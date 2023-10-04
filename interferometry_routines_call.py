c36f400ad2f8ca0de5e9b39f3e7fd12e1b7f246b
awbrenem <78624201+awbrenem@users.noreply.github.com>
2023-10-04 16:35:35 -0400

    significant plotting improvements

--- interferometry_routines_call.py
+++ interferometry_routines_call.py
@@ -45,17 +45,22 @@ fspec, tspec, powercB = signal.spectrogram(wfB, fs, nperseg=nps,noverlap=nps/2,w
 #Get cross spectrum 
 #tchunk = 0.2  #sec
 tchunk = 0.05  #sec
-coh, phase, tchunks, freqs = correlation_analysis.cross_spectral_density_spectrogram(wfA,wfB,tdat,fs,tchunk,coh_min=0.,nperseg=512)
+
+cohmin = 0.5  #Best to limit bad coherence values at the onset. Otherwise get a lot of salt/pepper noise in final result
+coh, phase, tchunks, freqs = correlation_analysis.cross_spectral_density_spectrogram(wfA,wfB,
+                                                                                     tdat,fs,tchunk,
+                                                                                     coh_min=cohmin,
+                                                                                     nperseg=512)
 
 
 #Reduce data to time range of interest [tz to tz+nsec]. 
 #(e.g. select wave packet of interest)
 #--Bernstein waves on upleg
-#tz = 140 
-#nsec = 3
+tz = 140 
+nsec = 6
 #--unknown power just prior to two-stream
-tz = 882  
-nsec = 4
+#tz = 882  
+#nsec = 4
 #--attitude adjustment maneuver (returns noise due to no coherence)
 #tz = 595  
 #nsec = 2
@@ -80,38 +85,23 @@ fkpowspec, kvals, fvals = interf.inter_fvsk(np.abs(powarr),tpowarr,fspec,
                                          klim=[-4,4])
 
 
+
 #Plot the results (freq vs k)
 vr = [-45,-20]
-yr = [0,8000]
-kr = [-4,4]
+yr = [5000,8000]
+kr = [-3,3]
 titlegoo = 'slice from '+ str(tz) + '-' + str(tz + nsec) + ' sec'
 xr = [tz-20,tz+20]
 
 fig,axs = plt.subplots(4)
 ps.plot_spectrogram(tspec,fspec,np.abs(powercA),vr=vr,yr=yr,xr=xr, yscale='linear',ax=axs[0],xlabel='time(s)',ylabel='power\nf(Hz)',title=titlegoo)
 ps.plot_spectrogram(tchunks,freqs,coh,vr=[0,1],zscale='linear',xr=xr,yr=yr,yscale='linear',ax=axs[1],xlabel='time(s)',ylabel='Coherence**2\nf(Hz)')
-ps.plot_spectrogram(tchunks,freqs,phase,vr=[0,140],zscale='linear',xr=xr,yr=yr,yscale='linear',ax=axs[2],xlabel='time(s)',ylabel='Phase\nf(Hz)')
+ps.plot_spectrogram(tchunks,freqs,np.abs(phase),vr=[0,180],zscale='linear',xr=xr,yr=yr,yscale='linear',ax=axs[2],xlabel='time(s)',ylabel='|Phase|(0-180deg)\nf(Hz)',cmap='PiYG')
 ps.plot_spectrogram(kvals,freqs,fkpowspec,vr=vr,xr=kr,yr=yr,yscale='linear',ax=axs[3],minzval=-120,maxzval=10,xlabel='k(rad/m)',ylabel='f(Hz)')
 for i in range(3): axs[i].axvline(tz)
 for i in range(3): axs[i].axvline(tz+nsec)
 
-
-
-vr = [-45,-20]
-yr = [100,1000]
-kr = [-4,4]
-titlegoo = 'slice from '+ str(tz) + '-' + str(tz + nsec) + ' sec'
-xr = [tz-20,tz+20]
-
-fig,axs = plt.subplots(4)
-ps.plot_spectrogram(tspec,fspec,np.abs(powercA),vr=vr,yr=yr,xr=xr, yscale='linear',ax=axs[0],xlabel='time(s)',ylabel='power\nf(Hz)',title=titlegoo)
-ps.plot_spectrogram(tchunks,freqs,coh,vr=[0,1],zscale='linear',xr=xr,yr=yr,yscale='linear',ax=axs[1],xlabel='time(s)',ylabel='Coherence**2\nf(Hz)')
-ps.plot_spectrogram(tchunks,freqs,phase,vr=[0,140],zscale='linear',xr=xr,yr=yr,yscale='linear',ax=axs[2],xlabel='time(s)',ylabel='Phase\nf(Hz)')
-ps.plot_spectrogram(kvals,freqs,fkpowspec,vr=vr,xr=kr,yr=yr,yscale='linear',ax=axs[3],minzval=-120,maxzval=10,xlabel='k(rad/m)',ylabel='f(Hz)')
-for i in range(3): axs[i].axvline(tz)
-for i in range(3): axs[i].axvline(tz+nsec)
 
 
 
 print('h')
-#axs[1].secondary_xaxis("bottom", functions=(k2,kvals))
