a83b63004fa7609714e430088cdea136949ff541
awbrenem <78624201+awbrenem@users.noreply.github.com>
2023-10-24 12:55:38 -0400

    minor updates

--- interferometry_fvsk_spec_test.py
+++ interferometry_fvsk_spec_test.py
@@ -5,31 +5,40 @@ NOTE: this technique only makes sense for parallel, spaced antennas.
 If the antennas are perpendicular, the ~90 deg phase shift in waves will be interpreted as a 
 large k-value. 
 
-TESTING: Results compare nearly exactly to a manual check I've done from antenna_finite_wavelength_response.py 
-on a 25 m wavelength wave at 50 Hz, propagating along the V4-V3 direction.
-SO, I THINK THIS ROUTINE WORKS FINE.  
+
+TESTING: See tests from antenna_finite_wavelength_response.py 
+
+NOTE: routine works great, with correct phase sense, with following two exceptions
+
+1) phase identification can be difficult if the wave is propagating nearly along 
+one of the interferometry axes (e.g. if along the y-hat' direction then V32 and V14 will
+measure very little signal, meaning the waveform can be noise-dominated)
+
+2) if wavelength is on the order or less than boom length than results can't be trusted.
 
 
-          V1
-       /  |  \             
-     x    |     x          
-   /      |       \        
-V3--------O--------V4     
-   \      |       /
-     x    |     x
-       \  |  /
-          V2
+
+                       (y-hat)
+                         V3
+                      /  |  \             
+           (y-hat') x    |     x (x-hat')         
+                  /      |       \        
+               V2--------O--------V1 (x-hat)    
+                  \      |       /
+                    x    |     x
+                      \  |  /
+                         V4
           
 
 x-points represent the centers of potential of the interferometry diagonals          
 
 Coordinate system (system of input test wave)
-x-hat --> V3 to V4 direction
-y-hat --> V2 to V1 direction
+x-hat --> E12 = V1 - V2 direction (positive to right)
+y-hat --> E34 = V3 - V4 direction (positive upwards)
 
 This code outputs the spectrum of k-values in the kx' and ky' directions, where
-x'-hat --> from V3V2 to V1V4 direction (45 deg inclined from x-hat)
-y'-hat --> from V4V2 to V1V3 direction (45 deg inclined from y-hat)
+x'-hat --> Ex' = V1V3x - V4V2x (45 deg inclined from x-hat)
+y'-hat --> Ey' = V3V2x - V1V4x (45 deg inclined from y-hat)
 
 """
 
@@ -56,22 +65,36 @@ import pickle
 #vBstr = 'VLF42D'
 
 #Load artificially generated test waveform
-goo = pickle.load(open('/Users/abrenema/Desktop/wavetst.pkl','rb'))
+#goo = pickle.load(open('/Users/abrenema/Desktop/wavetst.pkl','rb'))
+#goo = pickle.load(open('/Users/abrenema/Desktop/wavetst_f=50-200Hz.pkl','rb'))
+goo = pickle.load(open('/Users/abrenema/Desktop/wavetst_f=20-60Hz.pkl','rb'))
+
+
+#---Choose interferometry pair
+#NOTE: positive sense of phase defined as pointing towards center of potential of "wfA"
+#e.g. y-hat':  wfA = wf32; wfB = wf14
+#x-hat'
 #wfA = goo['wf13']
 #wfB = goo['wf42']
-wfA = goo['wf14']
-wfB = goo['wf32']
+#y-hat'
+wfA = goo['wf32']
+wfB = goo['wf14']
+
 tdat = goo['tvals']
 vals = goo['vals']
 fs = 1/(tdat[1]-tdat[0])
 
+
+
+
 wavelength = [vals['lambda_min'],vals['lambda_max']]
+#Values in antenna (non-primed) coordinates (see header)
 kx = vals['kx']
 ky = vals['ky']
 
 
-plt.plot(tdat,wfA)
-plt.xlim(0,0.05)
+#plt.plot(tdat,wfA)
+#plt.xlim(0,0.05)
 
 #Plot FFT
 #ft = np.fft.fft(wfA)
@@ -97,7 +120,7 @@ plt.plot(fspec,np.abs(powercA[:,10]))
 cohmin = 0.9  #Best to limit bad coherence values at the onset. Otherwise get a lot of salt/pepper noise in final result
 
 
-
+#NOTE: + sense of phase defined as pointing towards center of potential of "powercA"
 g,coh,phase = correlation_analysis.interferometric_coherence_2D(powercA,powercB,3)
 
 
@@ -105,19 +128,6 @@ g,coh,phase = correlation_analysis.interferometric_coherence_2D(powercA,powercB,
 goo = coh < cohmin
 coh[goo] = float("nan")
 phase[goo] = float("nan")
-
-#phase = np.degrees(phase)
-
-#yr = [0,500]
-#ps.plot_spectrogram(tspec,fspec,np.abs(np.degrees(phase)),vr=[0,5],zscale='linear',yr=yr,yscale='linear',xlabel='time(s)',ylabel='|Phase|(0-180deg)\nf(Hz)',cmap='Spectral')
-
-
-
-##Reduce arrays to desired timerange
-#goo, powercAz, tspecz = ps.slice_spectrogram(tz,tspec,np.abs(powercA),nsec)
-#goo, powercBz, tpowercBz = ps.slice_spectrogram(tz,tspec,np.abs(powercA),nsec)
-#goo, cohz, ttmp = ps.slice_spectrogram(tz,tspec,coh,nsec)
-#goo, phasez, ttmp = ps.slice_spectrogram(tz,tspec,phase,nsec)
 
 
 
@@ -144,11 +154,11 @@ fkpowspec, kvals, fvals = interf.inter_fvsk(np.abs(powercA),tspec,fspec,
 
 
 #Find k-location of max power for every frequency
-#---QUITE SURE THIS LOOP IS WORKING
 pmaxvals = np.zeros(np.shape(fkpowspec))
 pmaxvals[:,:] = 1
 for f in range(len(fspec)):
-    cond = fkpowspec[f,:] == np.nanmax(fkpowspec[f,:])
+    if np.nanmax(fkpowspec[f,:]):
+        cond = fkpowspec[f,:] == np.nanmax(fkpowspec[f,:])
     pmaxvals[f,:] = pmaxvals[f,:]*cond
 
 
@@ -164,7 +174,6 @@ fig,axs = plt.subplots(6)
 ps.plot_spectrogram(tspec,fspec,np.abs(powercA),vr=vr,yr=yr, yscale='linear',ax=axs[0],xlabel='time(s)',ylabel='power\nf(Hz)',title=titlegoo)
 ps.plot_spectrogram(tspec,fspec,np.abs(powercB),vr=vr,yr=yr, yscale='linear',ax=axs[1],xlabel='time(s)',ylabel='power\nf(Hz)')
 ps.plot_spectrogram(tspec,fspec,coh,vr=[0,1],zscale='linear',yr=yr,yscale='linear',ax=axs[2],xlabel='time(s)',ylabel='Coherence**2\nf(Hz)')
-#ps.plot_spectrogram(tspec,fspec,np.abs(phase),vr=[0,180],zscale='linear',xr=xr,yr=yr,yscale='linear',ax=axs[3],xlabel='time(s)',ylabel='|Phase|(0-180deg)\nf(Hz)',cmap='PiYG')
 ps.plot_spectrogram(tspec,fspec,np.abs(np.degrees(phase)),vr=[0,5],zscale='linear',yr=yr,yscale='linear',ax=axs[3],xlabel='time(s)',ylabel='|Phase|(0-180deg)\nf(Hz)',cmap='Spectral')
 ps.plot_spectrogram(kvals,fvals,fkpowspec,xr=kr,yr=yr,vr=vr,yscale='linear',ax=axs[4],minzval=0,maxzval=np.nanmax(fkpowspec),xlabel='k(rad/m)',ylabel='f(Hz)')
 ps.plot_spectrogram(kvals,fvals,pmaxvals,vr=[0,2],xr=kr,yr=yr,zscale='linear',yscale='linear',ax=axs[5],minzval=-120,maxzval=10,xlabel='k(rad/m)',ylabel='f(Hz)',cmap='Greys')
@@ -183,7 +192,6 @@ print('h')
 
 
 
-print('h')
 
 
 
