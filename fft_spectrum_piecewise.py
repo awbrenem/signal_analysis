9cfa8d7126f359762f5e456ce0737c704275c80c
awbrenem <78624201+awbrenem@users.noreply.github.com>
2025-06-04 11:37:49 -0400

    Add files via upload
    Modified returned freq spectrum to default to the median value for all times.

--- fft_spectrum_piecewise.py
+++ fft_spectrum_piecewise.py
@@ -10,9 +10,19 @@ fs_thres --> fraction of the median sample rate that current sample rate can be 
             e.g. fs_thres = 0.02 (i.e. 2%) means that sample rates < 98% of the median sample rate or > 102% are not considered. 
             Spectral data at these times will be NaN'd.
 
+full_freqs --> returns a freq spectrum for each time. Since the sample rate can vary, so do the freqs. 
+                However, attempting to plot a spectrogram with these will essentially crash Python. 
+                If not set, returns the median (over all times) frequencies 
+            
+
+Returns:     freqs_fin --> [f] (or [f,t] if 'full_freqs' set)  (final freqs)
+             tcenter_fin --> [t] (final time values - center of bin)
+             spec_fin --> [f,t]  (final sonogram values)
+             fs_fin --> [t]    (sample rate at each time chunk)
+             
 """
 
-def fft_spectrum_piecewise(times, data, nfft=512, noverlap=8, fs_thres=0.02):
+def fft_spectrum_piecewise(times, data, nfft=512, noverlap=8, fs_thres=0.02, full_freqs=0):
 
     import sys 
     sys.path.append('/Users/abrenema/Desktop/code/Aaron/github/mission_routines/rockets/Endurance/')
@@ -65,6 +75,21 @@ def fft_spectrum_piecewise(times, data, nfft=512, noverlap=8, fs_thres=0.02):
         #define time bins
         tcenter_fin[i] = (ttmp[-1] + ttmp[0])/2.
         tleft_fin[i] = ttmp[0]
+
+
+    #if you don't need the freq arrays for each time, average the frequencies. 
+    fmedian = np.zeros(int(nfft/2 + 1))
+
+    if not full_freqs:
+        for f in range(len(fmedian)):
+            fmedian[f] = np.median(freqs_fin[f,:])
+        freqs_fin = fmedian
+
+    #Test the median freqs vs freqs at a specific time
+    #plt.plot(freqs_fin[:,10])
+    #plt.plot(fmedian[:])
+    #df = freqs_fin[:,100] - fmedian
+    #plt.plot(df)
 
 
     return freqs_fin, tcenter_fin, spec_fin, fs_fin
